95263110121b6096e2f74101f712a23e0828aa8c
Brendon Feitosa <brendonfeitosa@hotmail.com>
2023-11-21 23:39:59 -0300

    Update index.js

--- curso/src/index.js
+++ curso/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
+import App from App;
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Atividades 2º Bimestre</h1>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
